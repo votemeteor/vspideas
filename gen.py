6e32841b53e58743c9e300620c1752c0d86ea22d
votemeteor <59272814+votemeteor@users.noreply.github.com>
2022-04-28 19:53:06 +1000

    Update gen.py

--- gen.py
+++ gen.py
@@ -3,10 +3,10 @@
 import random
 import time
 # Put words here (._.)
-start  =  ["example ", "example2 "]
-middle = ["example3 ", "foo "]
-middle2 = ["foo2 ", "foo3 " ]
-end = ["foo4", "foo5"]
+start  =  ["Indigenous ", "Sustainable "]
+middle = ["wine ", "dog "]
+middle2 = ["food ", "beverage " ]
+end = ["startup", "influencer campaign"]
 while True:
     # Sleep for 100ms before running code ('o')
     time.sleep(0.1)
